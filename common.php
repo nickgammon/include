9879039d9af27e9d46d8dce13a6b26024065ca41
Nick Gammon <nick@gammon.com.au>
2014-03-20 16:01:55 +1100

    Undid magic quotes if they are on for that server

--- common.php
+++ common.php
@@ -80,7 +80,20 @@ $DAYS_IN_MONTHS = array
   );  // end of array
   
 $debugInfo = array ();
+
+// fix up magic quotes, <sigh>
   
+if (get_magic_quotes_gpc()) {
+    function stripslashes_gpc(&$value)
+    {
+        $value = stripslashes($value);
+    }
+    array_walk_recursive($_GET, 'stripslashes_gpc');
+    array_walk_recursive($_POST, 'stripslashes_gpc');
+    array_walk_recursive($_COOKIE, 'stripslashes_gpc');
+    array_walk_recursive($_REQUEST, 'stripslashes_gpc');
+}
+
 function DefaultColours ()
   {
   global $colours;

86e6f6d1170480c9cc74e39d3d067a34c945d490
Nick Gammon <nick@gammon.com.au>
2016-09-12 08:39:26 +1000

    Added ValidateEmail function

--- common.php
+++ common.php
@@ -4343,4 +4343,53 @@ function duration ($days)
 
   } // end of duration
 
+function validateEmail ($email)
+  {
+
+// See: http://nikic.github.io/2012/06/15/The-true-power-of-regular-expressions.html
+
+$regexp = <<< EOD
+(?(DEFINE)
+  (?<addr_spec> (?&local_part) @ (?&domain) )
+  (?<local_part> (?&dot_atom) | (?&quoted_string) | (?&obs_local_part) )
+  (?<domain> (?&dot_atom) | (?&domain_literal) | (?&obs_domain) )
+  (?<domain_literal> (?&CFWS)? \[ (?: (?&FWS)? (?&dtext) )* (?&FWS)? \] (?&CFWS)? )
+  (?<dtext> [\x21-\x5a] | [\x5e-\x7e] | (?&obs_dtext) )
+  (?<quoted_pair> \\ (?: (?&VCHAR) | (?&WSP) ) | (?&obs_qp) )
+  (?<dot_atom> (?&CFWS)? (?&dot_atom_text) (?&CFWS)? )
+  (?<dot_atom_text> (?&atext) (?: \. (?&atext) )* )
+  (?<atext> [a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+ )
+  (?<atom> (?&CFWS)? (?&atext) (?&CFWS)? )
+  (?<word> (?&atom) | (?&quoted_string) )
+  (?<quoted_string> (?&CFWS)? " (?: (?&FWS)? (?&qcontent) )* (?&FWS)? " (?&CFWS)? )
+  (?<qcontent> (?&qtext) | (?&quoted_pair) )
+  (?<qtext> \x21 | [\x23-\x5b] | [\x5d-\x7e] | (?&obs_qtext) )
+
+  # comments and whitespace
+  (?<FWS> (?: (?&WSP)* \r\n )? (?&WSP)+ | (?&obs_FWS) )
+  (?<CFWS> (?: (?&FWS)? (?&comment) )+ (?&FWS)? | (?&FWS) )
+  (?<comment> \( (?: (?&FWS)? (?&ccontent) )* (?&FWS)? \) )
+  (?<ccontent> (?&ctext) | (?&quoted_pair) | (?&comment) )
+  (?<ctext> [\x21-\x27] | [\x2a-\x5b] | [\x5d-\x7e] | (?&obs_ctext) )
+
+  # obsolete tokens
+  (?<obs_domain> (?&atom) (?: \. (?&atom) )* )
+  (?<obs_local_part> (?&word) (?: \. (?&word) )* )
+  (?<obs_dtext> (?&obs_NO_WS_CTL) | (?&quoted_pair) )
+  (?<obs_qp> \\ (?: \x00 | (?&obs_NO_WS_CTL) | \n | \r ) )
+  (?<obs_FWS> (?&WSP)+ (?: \r\n (?&WSP)+ )* )
+  (?<obs_ctext> (?&obs_NO_WS_CTL) )
+  (?<obs_qtext> (?&obs_NO_WS_CTL) )
+  (?<obs_NO_WS_CTL> [\x01-\x08] | \x0b | \x0c | [\x0e-\x1f] | \x7f )
+
+  # character class definitions
+  (?<VCHAR> [\x21-\x7E] )
+  (?<WSP> [ \t] )
+)    # end of DEFINE
+^(?&addr_spec)$   # the actual validator
+EOD;
+
+  $delimiter = "\xFF";
+  return preg_match ($delimiter . $regexp . $delimiter . 'x', $email);
+  }   // end of validateEmail
 ?>

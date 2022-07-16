d7171a4147b3dd50f485d0470fa9f934957b8091
Nick Gammon <nick@gammon.com.au>
2022-07-17 08:21:51 +1000

    Added copy markdown to clipboard stuff

--- dtp.js
+++ dtp.js
@@ -1341,6 +1341,44 @@ function setSelections (event)
   return false;
   } // end of setSelections
 
+// see: https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
+
+// return a promise
+function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
+    }
+}
+
+
+function copyMarkdown ()
+{
+  var md_text = document.getElementById("markdown_text")
+
+  copyToClipboard(md_text.textContent)
+    .then(() => alert('Copied markdown to the Clipboard'))
+    .catch(() => alert('Cannot copy to Clipboard, select and copy it yourself.'));
+
+} // end of copyMarkdown
+
 
 // START HERE
 
